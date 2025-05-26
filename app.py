f30477d84039faa67ce00ea2f21ac09be8a3f632
yasunorikato <ykaq56@gmail.com>
2025-05-26 20:23:07 +0900

    first commit.

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-print("test" \
-"")
+import streamlit as st
+
+st.title("サンプルアプリ①: 簡単なWebアプリ")
+
+input_message = st.text_input(label="文字数のカウント対象となるテキストを入力してください。")
+
+text_count = len(input_message)
+
+if st.button("実行"):
+
+    st.write(f"文字数: **{text_count}**")
